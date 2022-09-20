691307731c7f84132a8a70eb4b86115413ea29ea
wat <twatcharin90@gamil.com>
2022-09-20 14:04:06 +0700

    feat: ex1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import { ChakraProvider } from "@chakra-ui/react";
+import { Stack, Button, Spinner, Input } from "@chakra-ui/react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <Button colorScheme="telegram" size="xs">
+        Button
+      </Button>
+      <Button colorScheme="messenger" size="sm">
+        Button
+      </Button>
+      <Button colorScheme="twitter" size="md">
+        Button
+      </Button>
+      <Stack direction="row" spacing={4}>
+        <Spinner size="xs" color="blue.500" />
+        <Spinner size="sm" color="red.500" />
+        <Spinner size="md" color="green.500" />
+      </Stack>
+
+      <Stack spacing={3}>
+        <Input variant="outline" placeholder="Outline" />
+        <Input variant="filled" placeholder="Filled" />
+        <Input variant="flushed" placeholder="Flushed" />
+        <Input variant="unstyled" placeholder="Unstyled" />
+      </Stack>
+    </ChakraProvider>
   );
 }
 
